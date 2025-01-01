06bcab4b9a35015e7a18cb90eeff53bdbfa8c506
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2025-01-01 22:37:10 +0100

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import {AuthProvider} from "@/app/providers";
+import {Header} from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Book Point",
@@ -16,6 +17,7 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
         <body>
         <AuthProvider>
+            <Header/>
             {children}
         </AuthProvider>
         </body>
