1cf8c796549c9de3ed08116bd766bb85c1f286e8
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2025-02-01 23:27:54 +0100

    remove unused book interface

--- src/actions/getBooks.ts
+++ src/actions/getBooks.ts
@@ -1,11 +1,3 @@
-'use server';
-import FetchWrapper from "@/lib/backendApi/fetchWrapper";
-import ApiConfig from "@/lib/backendApi/apiConfiguration";
-
-export const getBooks = async (page=0, size=10) => {
-    const api = new FetchWrapper();
-    return await api.get<BooksResponse>(`${ApiConfig.Endpoints.Books.All}?page=${page}&size=${size}`);
-}
 
 export interface Link {
     rel: string;
